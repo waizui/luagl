df280b98ef7ca60a300eba35c5f521a614861e21
leo <lch01234@gmail.com>
2024-10-04 23:52:51 +0900

    feat: install glfw glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,67 @@
+#include <glad/glad.h> // need load glad firstly then glfw
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <stdlib.h>
 
-void error_exist() {
+static void error_exist() {
   std::cout << "error init opengl";
   glfwTerminate();
+  abort();
 }
 
-void error_callback(int error, const char *description) {
+static void error_callback(int error, const char *description) {
   std::cout << "Error: " << description;
   error_exist();
 }
 
+static void key_callback(GLFWwindow *window, int key, int scancode, int action,
+                         int mods) {
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+static void framebuffer_size_callback(GLFWwindow *window, int width,
+                                      int height) {
+  glViewport(0, 0, width, height);
+}
+
 int main(int argc, char *argv[]) {
+
+  glfwSetErrorCallback(error_callback);
 
   if (!glfwInit()) {
     error_exist();
   }
 
-  glfwSetErrorCallback(error_callback);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+  int height{480};
+  int width{640};
+
+  GLFWwindow *window = glfwCreateWindow(width, height, "My Title", NULL, NULL);
+  if (!window) {
+    error_exist();
+  }
+  glfwMakeContextCurrent(window);
+
+  glfwSetKeyCallback(window, key_callback);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    error_exist();
+  }
+
+  glViewport(0, 0, width, height);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
   glfwTerminate();
-  std::cout << "finished luagl";
+  std::cout << "luagl terminate";
   return 0;
 }
