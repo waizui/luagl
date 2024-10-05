ee50afd474b043d446ceaa0e7fe3c385ee02e24f
leo <lch01234@gmail.com>
2024-10-05 21:14:42 +0900

    feat: combine lua with opengl

---
+++ src/luagl.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Luagl {
+
+void Testgl();
+
+}  // namespace Luagl
--- src/main.cpp
+++ src/main.cpp
@@ -1,145 +1,24 @@
-#include <glad/glad.h>  // need load glad firstly then glfw
-#include <GLFW/glfw3.h>
-#include <iostream>
-#include <stdlib.h>
-
-static void error_exist() {
-    std::cout << "error init opengl";
-    glfwTerminate();
-    abort();
-}
-
-static void error_callback(int error, const char* description) {
-    std::cout << "Error: " << description;
-    error_exist();
-}
-
-static void key_callback(GLFWwindow* window, int key, int scancode, int action,
-                         int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-    }
-}
-
-static void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
-}
-
-static void render_triangle(GLFWwindow* const window) {
-    // clang-format off
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
-    };
-    // clang-format on
-
-    // set shader
-    const char* vertexShaderSource =
-        R"(
-        #version 330 core
-        layout (location = 0) in vec3 aPos;
-        out vec4 verColor;
-        void main()
-        {
-           gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-           verColor = gl_Position;
-        }
-        )";
-
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    // TODO: check status
-
-    const char* fragmentShaderSource =
-        R"(
-        #version 330 core
-        out vec4 FragColor;
-        in vec4 verColor;
-        void main()
-        {
-        //  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-           FragColor = verColor;
-        }
-        )";
-
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    // A shader program object is the final linked version of multiple shaders combined.
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glUseProgram(shaderProgram);
-    // delete linked shader
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    // set VAO
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-
-    glBindVertexArray(VAO);
-    // set VBO
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // position:0, elements:3, type:GL_FLOAT,normalize:GL_FALSE,size:3*sizeof,start
-    // offset:nullptr
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
-    // 0:position as it's argument
-    glEnableVertexAttribArray(0);
-
-    while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.2, 0.5, 0.3, 1);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glUseProgram(shaderProgram);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+#include "luagl.h"
+extern "C" {
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
 }
 
 int main(int argc, char* argv[]) {
-    glfwSetErrorCallback(error_callback);
-
-    if (!glfwInit()) {
-        error_exist();
+    using Luagl::Testgl;
+    Testgl();
+    char buff[256];
+    int error;
+    lua_State* L = luaL_newstate(); /* opens Lua */
+    luaL_openlibs(L);               /* opens the standard libraries */
+    while (fgets(buff, sizeof(buff), stdin) != NULL) {
+        error = luaL_loadstring(L, buff) || lua_pcall(L, 0, 0, 0);
+        if (error) {
+            fprintf(stderr, "%s\n", lua_tostring(L, -1));
+            lua_pop(L, 1); /* pop error message from the stack */
+        }
     }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    int height{480};
-    int width{640};
-
-    GLFWwindow* window = glfwCreateWindow(width, height, "My Title", NULL, NULL);
-    if (!window) {
-        error_exist();
-    }
-    glfwMakeContextCurrent(window);
-
-    glfwSetKeyCallback(window, key_callback);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        error_exist();
-    }
-
-    glViewport(0, 0, width, height);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    render_triangle(window);
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    std::cout << "luagl terminate";
+    lua_close(L);
     return 0;
 }
